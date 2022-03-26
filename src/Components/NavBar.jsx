06331f2d6a483be25a63e9a0aa430ac17a8167bd
Matheus Nugas <matheusnugas@gmail.com>
2022-03-26 19:34:30 -0300

    test no img

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useContext } from "react";
 import CountriesContext from "../Context/CountriesContext";
 // import sunImg from "../Images/sun.png";
-import moonImg from "../Images/moon.png";
+// import moonImg from "../Images/moon.png";
 
 function NavBar() {
   const { darkMode, setDarkMode } = useContext(CountriesContext);
@@ -10,7 +10,7 @@ function NavBar() {
     <nav className={!darkMode ? "navBar" : "navBarDark"}>
       <h1 className="pageNavTitle">Where in the world?</h1>
       <button onClick={() => setDarkMode(!darkMode)} className="navToggler">
-        <img src={moonImg} alt="sunOrMoon" />
+        {/* <img src={moonImg} alt="sunOrMoon" /> */}
         {!darkMode ? "Dark Mode" : "Light Mode"}
       </button>
     </nav>
