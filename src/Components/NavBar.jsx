6921bceb294d3506ec785d720f88409f57e9cb6e
Matheus Nugas <matheusnugas@gmail.com>
2022-03-26 19:47:14 -0300

    removed images

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useContext } from "react";
 import CountriesContext from "../Context/CountriesContext";
-// import sunImg from "../Images/sun.png";
-// import moonImg from "../Images/moon.png";
+import sunImg from "../Images/whiteSun.png";
+import moonImg from "../Images/blackMoon.png";
 
 function NavBar() {
   const { darkMode, setDarkMode } = useContext(CountriesContext);
@@ -10,7 +10,7 @@ function NavBar() {
     <nav className={!darkMode ? "navBar" : "navBarDark"}>
       <h1 className="pageNavTitle">Where in the world?</h1>
       <button onClick={() => setDarkMode(!darkMode)} className="navToggler">
-        {/* <img src={moonImg} alt="sunOrMoon" /> */}
+        <img src={!darkMode ? moonImg : sunImg} alt="sunOrMoon" />
         {!darkMode ? "Dark Mode" : "Light Mode"}
       </button>
     </nav>
